036222c2b829d85d51bf9c59679294e852ce7060
Ramesh Vyas <rameshovyas@gmail.com>
2022-06-30 17:10:49 +0530

    Added a dynamc route

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,8 @@ import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/contact.tsx";
-import * as $4 from "./routes/index.tsx";
+import * as $4 from "./routes/greet/[name].tsx";
+import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -15,7 +16,8 @@ const manifest = {
     "./routes/about.tsx": $1,
     "./routes/api/joke.ts": $2,
     "./routes/contact.tsx": $3,
-    "./routes/index.tsx": $4,
+    "./routes/greet/[name].tsx": $4,
+    "./routes/index.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
