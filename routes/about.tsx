b84429423f0d6ae6555f13ca80285e8aa6c90a22
Ramesh Vyas <rameshovyas@gmail.com>
2022-06-30 13:31:35 +0530

    Initial Commit

---
+++ routes/about.tsx
@@ -0,0 +1,13 @@
+// routes/about.tsx
+
+/** @jsx h */
+import { h } from "preact";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>Hello, you are in our about us page.</p>
+    </main>
+  );
+}
---
+++ routes/contact.tsx
@@ -0,0 +1,13 @@
+// routes/contact.tsx
+
+/** @jsx h */
+import { h } from "preact";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h2>Contact Us</h2>
+      <p>Hello, you are in our contact us page.</p>
+    </main>
+  );
+}
